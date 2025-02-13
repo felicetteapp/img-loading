75c50c19acf5930a0c3cb894ee7455b6619d91d0
Facundo Leites <leitesfacundo@gmail.com>
2025-02-13 08:37:04 -0300

    chore: update .gitignore to exclude dist directory and remove generated TypeScript declaration files

--- dist/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { ImgLoading } from "./ImgLoading";
--- dist/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-export type Selector = string | HTMLElement | Element | NodeList | NodeListOf<Element> | HTMLCollection | Array<Element> | ArrayLike<Element> | null;
-export interface ImgLoadingOptions {
-    prefix?: string;
-    container?: HTMLElement;
-}
